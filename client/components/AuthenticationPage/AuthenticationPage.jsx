d283ba4a3a80197750cefb0c6fc21f3dddfe056d
Imneet Kaur <kaur007imneet@gmail.com>
2016-10-22 15:08:50 +0530

    Modified Folder Structure

--- client/components/AuthenticationPage/AuthenticationPage.jsx
+++ client/components/AuthenticationPage/AuthenticationPage.jsx
@@ -3,16 +3,21 @@ import RaisedButton from 'material-ui/RaisedButton';
 import {Link} from 'react-router';
 
 class AuthenticationPage extends React.Component{
-   render(){
-       return(
-       <div className='row center-xs'>
-           <Link to='/apps' style={{textDecoration:'none'}}>
-           <h3>Click on the below button to login to your GitHub Account</h3>
-           <RaisedButton label="Login with GitHub" type="submit" primary={true} />
-           </Link>
-           </div>
-           );
-   };
+  render(){
+      return(
+      <div className='row center-xs'>
+       <div className='col-xs-12 col-mg-12 col-lg-12'>  
+          <h3 style={{marginTop:'100px'}}>Click on the below button to login to your GitHub Account</h3>
+          <img src="./components/AuthenticationPage/images.png" />
+          <div className='row center-xs'>
+          <Link to='/apps' style={{textDecoration:'none'}}>
+          <RaisedButton label="Login with GitHub" type="submit" primary={true} />
+          </Link>
+          </div>
+          </div>
+          </div>
+          );
+  };
 };
 
 export default AuthenticationPage;
