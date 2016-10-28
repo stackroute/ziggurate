33b0d2c220f0477073c71f20767ff01372a17195
nischay30 <nischay.goyal@gmail.com>
2016-10-28 17:09:02 +0530

    Created Configuration

---
+++ server/test/config.spec.js
@@ -0,0 +1,12 @@
+require('should');
+describe("config",function(){
+
+ 	it("get Config",function(){
+ 		const config=require('../config');
+ 		config.GITHUB_CLIENT_ID.should.be.exactly("GITHUB_CLIENT_ID");
+ 		config.GITHUB_CLIENT_SECRET.should.be.exactly("GITHUB_CLIENT_SECRET");
+ 		config.GITHUB_ORGANISATION.should.be.exactly("GITHUB_ORGANISATION");
+ 		config.CONSUL_MASTER.should.be.exactly("CONSUL_MASTER");
+
+ 	})
+ })
