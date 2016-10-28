bc5b5ccddf95d1af87ab8b391268fd219ededd8f
nischay30 <nischay.goyal@gmail.com>
2016-10-29 00:57:55 +0530

    Ansible Provisioning

---
+++ Ansible/README.md
@@ -0,0 +1,5 @@
+# Just clone the repository and go to the Project Home folder and type the command in the terminal
+
+ansible-playbook -i hosts appFabric.yml 
+
+#Before this command make sure your ssh-id is copied into the destination machines
---
+++ Ansible/files/dockerRegisteryConsul/dockerMaster/dockerService.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e "{\"ID\":\"docker$(hostname)\",\"Name\":\"DockerMaster\", \"Address\":\"$(ifconfig enp0s8 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)\", \"Port\":2375,\"Check\":{\"Interval\":\"10s\",\"TTL\":\"15s\"}}" > /etc/docker.json
