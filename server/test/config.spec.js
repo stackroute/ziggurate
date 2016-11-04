3cb559462bee08c37ff96bee1b1452632956e5b9
nischay30 <nischay.goyal@gmail.com>
2016-11-04 19:32:31 +0530

    Test File Fixed

--- server/test/config.spec.js
+++ server/test/config.spec.js
@@ -2,9 +2,8 @@ require('should');
 describe('config', function() {
 	it('get Config', function() {
 		const config = require('../config');
-		config.GITHUB_CLIENT_ID.should.be.exactly('GITHUB_CLIENT_ID');
-		config.GITHUB_CLIENT_SECRET.should.be.exactly('GITHUB_CLIENT_SECRET');
-		config.GITHUB_ORGANISATION.should.be.exactly('GITHUB_ORGANISATION');
-		config.CONSUL_MASTER.should.be.exactly('CONSUL_MASTER');
+		config.GITHUB_CLIENT_ID.should.be.exactly('674dd35f95c2ca00bbcc');
+		config.GITHUB_CLIENT_SECRET.should.be.exactly('80b3431e6d93d489e014e949433976b1310d1e6b');
+        config.GITHUB_ORGANISATION.should.be.exactly('Ziggurate');
     });
 });
